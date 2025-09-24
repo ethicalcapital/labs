a72a57c1c7a02e58797573c00b7fd468574820cb
Sloane <sloane@srvo.org>
2025-09-24 16:16:30 -0600

    Set up linting and fix simulator script

---
+++ eslint.config.cjs
@@ -0,0 +1,46 @@
+const js = require("@eslint/js");
+const { browser: browserGlobals } = require("globals");
+const react = require("eslint-plugin-react");
+const reactHooks = require("eslint-plugin-react-hooks");
+const jsxA11y = require("eslint-plugin-jsx-a11y");
+
+module.exports = [
+  {
+    ignores: ["node_modules/**"],
+  },
+  js.configs.recommended,
+  {
+    files: ["**/*.jsx"],
+    languageOptions: {
+      parser: require("@babel/eslint-parser"),
+      parserOptions: {
+        requireConfigFile: false,
+        babelOptions: {
+          presets: ["@babel/preset-react"],
+        },
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        ...browserGlobals,
+        React: "readonly",
+        ReactDOM: "readonly",
+        Chart: "readonly",
+      },
+    },
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+      "jsx-a11y": jsxA11y,
+    },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+      "react/jsx-filename-extension": ["error", { extensions: [".jsx"] }],
+      "react/prop-types": "off",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+    },
+  },
+];
